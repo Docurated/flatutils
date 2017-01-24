a564b891ec725438e458d58bc4916df56cad79ad
Sam Paul <sam@docurated.com>
2017-01-24 10:49:07 -0500

    upping version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="flatutils",
-    version="1.0.7",
+    version="1.0.8",
     description="Simple, lightweight utility to sort and iterate through large pg_dumps",
     author="Adam Duston",
     author_email="adam@docurated.com",
